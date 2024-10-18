f0c2478e1360b45460e854b7caddd1164f7e6d1d
RubenEscobar14 <ruben.escobar.c14@gmail.com>
2024-10-18 15:39:09 -0500

    Did pt 1, started pt 2 (nothing running yet), and mysterious other changes

--- java-type-checker/java_type_checker/types.py
+++ java-type-checker/java_type_checker/types.py
@@ -88,6 +88,10 @@ class JavaPrimitiveType(JavaType):
 
     Primitive types are not object types and do not have methods.
     """
+    def is_subtype_of(self, other):
+        if (other == self):
+            return True
+        return False
 
 
 class JavaObjectType(JavaType):
@@ -132,6 +136,15 @@ class JavaObjectType(JavaType):
                 except NoSuchJavaMethod:
                     pass
             raise NoSuchJavaMethod("{0} has no method named {1}".format(self.name, name))
+        
+    def is_subtype_of(self, other):
+        # if self.direct_supertypes.__contains__(other):
+        #     return True
+        if self == other:
+            return True
+        for thing in self.direct_supertypes:
+            return thing.is_subtype_of(other)
+        return False
 
 
 class JavaVoidType(JavaType):
--- python-attr-lookup/src/plang/PythonObject.java
+++ python-attr-lookup/src/plang/PythonObject.java
@@ -68,7 +68,11 @@ public class PythonObject {
      * @throws PythonAttributeException When there is no attribute on this object with that name.
      */
     public final PythonObject get(String attrName) throws PythonAttributeException {
-        throw new UnsupportedOperationException("not implemented yet");
+        for (PythonObject object : getMRO()){
+            if (object.attrs.containsKey(attrName))
+                return object.attrs.get(attrName);
+        }
+        throw new PythonAttributeException(this, attrName);
     }
 
     /**
@@ -80,7 +84,7 @@ public class PythonObject {
      * @param value Its new value
      */
     public final void set(String attrName, PythonObject value) {
-        throw new UnsupportedOperationException("not implemented yet");
+        attrs.put(attrName, value);
     }
 
     @Override
